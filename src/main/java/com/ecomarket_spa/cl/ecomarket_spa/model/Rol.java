09ee16451e8159df53066cb3895aba69012d77c8
vice. <vice.alvarezm@duocuc.cl>
2025-07-02 11:18:47 -0400

    Creacion de prueba RolRepositoryTest, funcionando correctamente

--- src/main/java/com/ecomarket_spa/cl/ecomarket_spa/model/Rol.java
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/model/Rol.java
@@ -1,11 +1,15 @@
 package com.ecomarket_spa.cl.ecomarket_spa.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "roles")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rol {
 
     @Id
